650edb066286bcbc4c82d5cdbe24577c2a4671c4
robertsrosa15@gmail.com <robertsrosa15@gmail.com>
2016-11-30 18:46:12 -0500

    Real time data animation is working fine. Still need to integrate the direction service to make the animation smoother.                                               Need to fix initial page with routes and what not.                                               Have not tested the scheduled data animation.

--- app/src/main/java/com/rsquared/robert/bustrackri/MainActivity.java
+++ app/src/main/java/com/rsquared/robert/bustrackri/MainActivity.java
@@ -29,15 +29,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-   /*     fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -65,8 +56,6 @@ public class MainActivity extends AppCompatActivity
         populateMenu(menu, busRoutesArray);
 
         navigationView.setNavigationItemSelectedListener(this);
-
-//        startMap();
 
     }
 
@@ -139,7 +128,7 @@ public class MainActivity extends AppCompatActivity
         try {
             WebView webView = (WebView) findViewById(R.id.webView);
             webView.setWebViewClient(new WebViewClient(this));
-        webView.loadUrl("http://m.ripta.com/1");
+            webView.loadUrl("http://m.ripta.com/1");
 //            webView.loadUrl("http://localhost:8080/UIStevens/UI_Project_1/Ripta/index.html");
         }catch (Exception e){
             e.printStackTrace();
